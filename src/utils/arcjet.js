522b8037133b07178ff77a545e03cd473455c0d4
suvasis-patra <suvasis02@kgpian.iitkgp.ac.in>
2026-04-14 11:44:48 +0530

    fix typo at arcjet config file

--- src/utils/arcjet.js
+++ src/utils/arcjet.js
@@ -3,10 +3,6 @@ import arcjet, { detectBot, shield, slidingWindow } from "@arcjet/node";
 const arcjetKey = process.env.ARCJET_KEY;
 const arcjetMode = process.env.ARCJET_MODE === "DRY_RUN" ? "DRY_RUN" : "LIVE";
 
-if (!arcjetKey) {
-  throw new Error("ARCJET_KEY environment variable is missing");
-}
-
 export const httpArcjet = arcjetKey
   ? arcjet({
       key: arcjetKey,
